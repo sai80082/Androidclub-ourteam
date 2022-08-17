b200acfa62ea3bbcab4f8bd7ce1d1482a7f86b2f
Sasank.D <98147894+dsasank@users.noreply.github.com>
2022-08-17 15:31:36 +0530

    Add files via upload

---
+++ alumni.jsx
@@ -0,0 +1,39 @@
+const alumni=[
+    {
+        name:"Abhishek Soni",
+        desig:"Ex-President",
+        img:"https://androidclubvit.com/images/avatars/our_team/Abhishek.jpeg",
+        desc:"He has been a part of this club since his first year, got the opportunity to learn a lot from our seniors, and has imbibed many of their qualities. He doesn’t speak much but his actions surely speak volumes. His calm and steady nature has inspired and held the club together. Maybe that’s why his favorite quote is “let’s meditate”."
+    },
+    {
+        name:"Neelesh ranjan Jha",
+        desig:"Ex-Vice President",
+        img:"	https://androidclubvit.com/images/avatars/our_team/neelesh.jpg",
+        desc:"2 AM or 2 PM, he is always there to solve any issue you have. He makes your errors his own so feel free to annoy the hell out of him (PS I hope he doesn’t get angry at you).His knowledge is way beyond the boundaries of Tech so don’t mistake him for just a nerdy geek. "
+    },
+    {
+        name:"Ritesh",
+        desig:"Ex-Vice President Management",
+        img:"	https://androidclubvit.com/images/avatars/our_team/Ritesh.jpg",
+        desc:"He could be described as one of the most zestful members of our core. His expertise has impressed many(hopefully). One day you will find him coding the other day designing a poster. He doesn't believe in mediocrity so gear up and give your best. The best way to bribe him is with a bottle of “Thums UP” and maybe play valorant with him."
+    },
+    {
+        name:"Shreya Roy",
+        desig:"Ex-General Secretary",
+        img:"	https://androidclubvit.com/images/avatars/our_team/Shraddha.jpeg",
+        desc:"She tries to make sense of all the chaos around us. Her patience is beyond our  understanding. It’s not that easy to infuriate her so if she is angry at you “What have you done??”. She is basically an introvert but once you get to know her she is really an amazing person and maybe a tad bit annoying (just kidding). ",
+    },
+    {
+        name:"Shrimayee Deshpande",
+        desig:"Ex-WorkForce Lead",
+        img:"https://androidclubvit.com/images/avatars/our_team/Shrimayee.jpeg",
+        desc:"She is one of the sweetest people you will ever meet but don’t ever think that’s her  weakness cause well if you get on her bad side, Oops!!. She is brilliant at multitasking, solving complex issues is like a piece of cake for her. Hard work and dedication have got her where she is now and that’s what keeps her grounded to her roots."
+    },
+    {
+        name:"Naman Agarwal",
+        desig:"Ex-Tech Lead",
+        img:"	https://androidclubvit.com/images/avatars/our_team/naman.jpg",
+        desc:" His grasp on technology is just great( NO PUN INTENDED). There is nothing he doesn’t have the answer to, and if he doesn’t he will surely find the answer and get back to you. Apart from coding, Life for him is all about quoting The Office and hence, “Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way"
+    }
+]
+export default alumni
