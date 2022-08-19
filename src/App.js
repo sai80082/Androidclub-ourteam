7cb6f41f6da5e84b60187de79df256a11cba23c7
sai charan guntupalli <saicharan.guntupalli@gmail.com>
2022-08-19 23:33:08 +0530

    add styling to the files

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import alu from "./alumni.jsx";
 function App() {
   return (
     <div>
-      <div className="bg-zinc-900">
-      <h1 className="text-xl text-white" >Our Team</h1>
+      <div className="bg-zinc-900 w-5/6 h-1/2 rounded-[64px] mx-auto my-4 p-4">
+        <h1 className=" text-white text-center text-5xl font-bold">Our Team</h1>
         {imag.map((ele) => {
           return (
-            <div className="rounded-md bg-white m-20 w-40">
+            <div className="rounded-[18px] bg-white m-20 w-40">
               <div className="container mx-auto gap-1">
                 <img
                   src={ele.img}
@@ -27,24 +27,27 @@ function App() {
           );
         })}
       </div>
-
-      <div className="text-xl">our alumni</div>
-      {alu.map((ele) => {
-        return (
-          <>
-            <div className="ccontainer mx-auto grid grid-cols-2 gap-1">
-              <img
-                src={ele.img}
-                alt="this is image"
-                className="h-20 w-20 rounded-full"
-              ></img>
-              <p>{ele.name}</p>
-              <p>{ele.desig}</p>
-              <p>{ele.desc}</p>
-            </div>
-          </>
-        );
-      })}
+      <div className="bg-zinc-900 w-5/6 h-1/2 rounded-[64px] mx-auto my-4 p-4">
+        <div className=" text-white text-center text-5xl font-bold">
+          our alumni
+        </div>
+        {alu.map((ele) => {
+          return (
+            <>
+              <div className="rounded-[18px] bg-white m-20 w-40">
+                <img
+                  src={ele.img}
+                  alt="this is image"
+                  className="h-20 w-20 rounded-full"
+                ></img>
+                <p>{ele.name}</p>
+                <p>{ele.desig}</p>
+                <p>{ele.desc}</p>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
